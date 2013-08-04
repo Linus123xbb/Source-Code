bbc1990461d372cc9fcd26e90d3baec1adbbdc4e
Linus123xbb <linus969@home.se>
2013-08-05 00:45:46 +0200

    EntityD.java
    EntityD

---
+++ EntityD.java
@@ -0,0 +1,14 @@
+package poop.linus.poopgame.entities.classes;
+
+import java.awt.Graphics;
+import java.awt.Rectangle;
+
+public interface EntityD {
+
+  public void tick();
+	public void render(Graphics g);
+	public Rectangle getBounds();
+	
+	public double getX();
+	public double getY();
+}
