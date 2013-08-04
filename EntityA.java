45957ff187d5bdbd5f2987411a06a37bf8f7da9c
Linus123xbb <linus969@home.se>
2013-08-05 00:43:58 +0200

    EntityA.java
    EntityA

---
+++ EntityA.java
@@ -0,0 +1,18 @@
+package poop.linus.poopgame.entities.classes;
+
+import java.awt.Graphics;
+import java.awt.Rectangle;
+
+public interface EntityA {
+
+  public void tick();
+	public void render(Graphics g);
+	public Rectangle getBounds();
+	
+	public double getX();
+	public double getY();
+	
+	public void setVelY(double velY);
+	public void addVelY(double velY);
+	public void OnGround(boolean OnGround);
+}
